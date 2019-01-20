6c487d9bea3c6b2c345a426acc046bec9dd46a2e
KCLI <likwokc@mail2.sysu.edu.cn>
2019-01-20 05:06:21 -0800

    add comment

--- trackbar/trackbar.cpp
+++ trackbar/trackbar.cpp
@@ -6,27 +6,30 @@
 using namespace std;
 using namespace cv;
 
+/***************定义回调函数*************/
 void on_trackbar(int pos, void *data)
 {
 
-		Mat srcImage = *(Mat*)(data);
-		Mat dstImage;
+		Mat srcImage = *(Mat*)(data);  //强制类型转换
+		Mat dstImage;  
 
-		threshold(srcImage, dstImage, pos, 255, 0);
+		threshold(srcImage, dstImage, pos, 255, 0);  //二值化
 		imshow("threshold", dstImage);
 }
 
 int main(int argc, char* argv[])
 {
-		Mat srcImage = imread("666.jpeg");
+		Mat srcImage = imread("666.jpeg");  //读入数据
+
+		/****判断图片是否读入成功****/
 		if (srcImage.empty())
 		{
 				cout << "LOAD IMAGE FAILURE" << endl;
 				return -1;
 		}
-
+		
 		Mat Gray;
-		cvtColor(srcImage, Gray, CV_RGB2GRAY);
+		cvtColor(srcImage, Gray, CV_RGB2GRAY);//灰度化
 		namedWindow("threshold");
 		imshow("threshold", Gray);
 
